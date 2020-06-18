f1f79ca1ffcad229d21329313dce11639af9c183
Sufyan Khot <khotsufyan@gmail.com>
2020-06-18 14:37:12 +0530

    UI fixed

--- app.py
+++ app.py
@@ -56,4 +56,5 @@ def predict():
 if __name__ == "__main__":
     app.config['DEBUG'] = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
     app.run(debug=True)
